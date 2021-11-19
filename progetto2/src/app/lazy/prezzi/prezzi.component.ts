97c97c0a3294f256d003ff8c24c8ce2dab8b3368
Giovamagno <giovamagno@yahoo.it>
2021-11-19 11:39:45 +0100

    finita pagina prezzi

--- progetto2/src/app/lazy/prezzi/prezzi.component.ts
+++ progetto2/src/app/lazy/prezzi/prezzi.component.ts
@@ -9,13 +9,18 @@ export class PrezziComponent implements OnInit {
   qualevedo:any;
   testo=[
     '<div><h4>hosting based:</h4><ul><li>+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi aliquam quidem ut veritatis similique, atque, illo quisquam culpa, repellat nobis laborum beatae laboriosam. Necessitatibus, mollitia velit nulla illo obcaecati nesciunt!</li><li>+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi aliquam quidem ut veritatis similique, atque, illo quisquam culpa, repellat nobis laborum beatae laboriosam. Necessitatibus, mollitia velit nulla illo obcaecati nesciunt!</li><li>+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi aliquam quidem ut veritatis similique, atque, illo quisquam culpa, repellat nobis laborum beatae laboriosam. Necessitatibus, mollitia velit nulla illo obcaecati nesciunt!</li></ul></div>',
+    '<div><h4>hosting avg:</h4><ul><li>+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi aliquam quidem ut veritatis similique, atque, illo quisquam culpa, repellat nobis laborum beatae laboriosam. Necessitatibus, mollitia velit nulla illo obcaecati nesciunt!</li><li>+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi aliquam quidem ut veritatis similique, atque, illo quisquam culpa, repellat nobis laborum beatae laboriosam. Necessitatibus, mollitia velit nulla illo obcaecati nesciunt!</li><li>+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi aliquam quidem ut veritatis similique, atque, illo quisquam culpa, repellat nobis laborum beatae laboriosam. Necessitatibus, mollitia velit nulla illo obcaecati nesciunt!</li></ul></div>',
+    '<div><h4>hosting top:</h4><ul><li>+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi aliquam quidem ut veritatis similique, atque, illo quisquam culpa, repellat nobis laborum beatae laboriosam. Necessitatibus, mollitia velit nulla illo obcaecati nesciunt!</li><li>+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi aliquam quidem ut veritatis similique, atque, illo quisquam culpa, repellat nobis laborum beatae laboriosam. Necessitatibus, mollitia velit nulla illo obcaecati nesciunt!</li><li>+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi aliquam quidem ut veritatis similique, atque, illo quisquam culpa, repellat nobis laborum beatae laboriosam. Necessitatibus, mollitia velit nulla illo obcaecati nesciunt!</li></ul></div>'
   ]
   constructor() { }
   cliccato(put:any){
-
-
+    this.qualevedo=put;
+  }
+  test(){
+    return this.qualevedo;
   }
   ngOnInit(): void {
+    console.log(this.qualevedo);
   }
 
 }
