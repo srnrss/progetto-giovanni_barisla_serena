7d48ee970ba6f8282b6e0108366cf29c20cc361d
Giovamagno <giovamagno@yahoo.it>
2021-11-19 10:00:16 +0100

    aggiunto modulo lazy, manca ancora il router lazy

---
+++ progetto/src/app/lazy/prezzi/prezzi.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PrezziComponent } from './prezzi.component';
+
+describe('PrezziComponent', () => {
+  let component: PrezziComponent;
+  let fixture: ComponentFixture<PrezziComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PrezziComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PrezziComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ progetto/src/app/lazy/prezzi/prezzi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prezzi',
+  templateUrl: './prezzi.component.html',
+  styleUrls: ['./prezzi.component.css']
+})
+export class PrezziComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
