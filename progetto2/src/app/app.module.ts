56eec1ed6c3281e49c86c0b98952574306efd1e1
serena <serena.russo8@studio.unibo.it>
2021-11-19 11:28:49 +0100

    login (non ancora funzionante)

--- progetto2/src/app/app.module.ts
+++ progetto2/src/app/app.module.ts
@@ -15,7 +15,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent,HeaderComponent,FooterComponent]
--- progetto2/src/app/lazy/login/login.component.ts
+++ progetto2/src/app/lazy/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,5 +12,6 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
 
 }
