7669b456ee640c6ccab962c06b6578399efc6831
serena <serena.russo8@studio.unibo.it>
2021-11-19 12:42:12 +0100

    login collegato ad admin + gestione errori

--- progetto2/src/app/app.module.ts
+++ progetto2/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent,HeaderComponent,FooterComponent]
--- progetto2/src/app/lazy/login/login.component.ts
+++ progetto2/src/app/lazy/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,11 +12,44 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
+
 
   ngOnInit(): void {
   }
 
+ 
+
+   //apertura pagina(richiamato in accesso())
+   apripagina(pagina:string):void{
+    this.router.navigate([`${pagina}`]);
+   }
+  
+   
+
+  //accesso pagina:
+  //user/0
+  //user/1
+  //user/2
+  accesso(){
+  var email_input= document.getElementById("email") as HTMLInputElement  
+  var password_input =document.getElementById("password") as HTMLInputElement
+
+   if(email_input.value===""||password_input.value===""){
+    alert("riempire tutti i campi")
+  }else if(email_input.value==="giovanni@gmail.com"&&password_input.value==="1234"){
+    this.apripagina("user/0")
+  }else if(email_input.value==="serena@gmail.com"&&password_input.value==="1234"){
+    this.apripagina("user/1")
+  }else if(email_input.value==="barisla@gmail.com" && password_input.value==="1234"){
+    this.apripagina("user/2")
+  }else {
+    alert("accesso negato")
+  }
+
+
+
+  }
   /*controllo info inserite
   onFormSubmit(userForm: NgForm) {
    console.log(userForm.value);
